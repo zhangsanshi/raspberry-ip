58c325faf26d1aa296fb81041b8f273977654d51
zhangsanshi <zhangsanshi5566@gmail.com>
2016-09-18 01:54:58 +0800

    'init'

---
+++ index.js
@@ -0,0 +1,47 @@
+var ip = require('ip');
+var child_process = require('child_process');
+var exec = child_process.exec;
+var fileQueue;
+
+function getFilePath (source) {
+    var sourcePath = './source/' + source + '.mp3';
+    return sourcePath;
+}
+
+function playAudio (loop) {
+    var currentFileQueue = fileQueue.shift();
+    fileQueue.push(currentFileQueue);
+    exec('omxplayer ' + currentFileQueue, function (err) {
+        if (!err) {
+            if (++loop < fileQueue.length) {
+                playAudio(loop);
+            } else {
+                play();
+            }
+        } else {
+            console.log(err);
+        }
+    });
+}
+
+function play () {
+    var address = ip.address();
+    var loop = 0;
+    fileQueue = [];
+    if (address) {
+        address = address.split('');
+        fileQueue.push(getFilePath('start'));
+        address.forEach(function (name, index) {
+            if (name === '.') {
+                name = 'point';
+            }
+            fileQueue.push(getFilePath(name));
+        });
+        fileQueue.push(getFilePath('end'));
+        playAudio(loop);
+    }
+}
+
+exec('cd ' + __dirname);
+
+play();
